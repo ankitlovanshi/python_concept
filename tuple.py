96e8c4fbd5671ea3b35c039b03022e9ce9c0d7df
ankitlovanshi <ankitlovanshi622@gmail.com>
2024-06-12 11:33:58 +0530

    operation on tuple

---
+++ tuple.py
@@ -0,0 +1,17 @@
+tuple.py
+# decleare tuple
+tuple1 = (4, 5, 6, 7, 8)
+print(tuple1)
+
+
+# index --> it will give the index of 4
+print(tuple1.index(4))
+
+
+# count --> count will be count of perticular number
+print(tuple1.count(4))
+
+
+# type casting
+my_list = list(tuple1)
+my_set = set(tuple1)
