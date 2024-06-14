63cba922cd12f1021e69bb07e1c24db5b0144987
ankitlovanshi <ankitlovanshi622@gmail.com>
2024-06-14 19:14:28 +0530

    delete some files

--- file_handling/write_csv.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('student.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Enl", "Name", "Branch"])
-    writer.writerow([201, "Ankit", "CSE"])
-    writer.writerow([202, "Aman", "CSE"])
-    
